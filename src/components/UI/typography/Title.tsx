6627c333e34ab8b4026966d0e2e4fdfa4f49ccb9
Hamed Osama <hamedosama19@gmail.com>
2023-06-15 03:10:49 +0300

    test build

--- src/components/UI/typography/Title.tsx
+++ src/components/UI/typography/Title.tsx
@@ -1,29 +1,30 @@
 import { FC } from 'react'
-import { cva, VariantProps } from 'class-variance-authority'
+// import { cva, VariantProps } from 'class-variance-authority'
 
 import { cn } from '@/utils/utils'
 
 
-const TitleVariants = cva(
-  'text-xl md:text-3xl lg:text-4xl font-bold',
-  {
-    variants: {
-      variant: {
-        default: 'text-primary text-start',
-        doubleBorder: '',
-      }
-    },
-    defaultVariants: {
-      variant: 'default',
-    }
-  }
-)
-interface TitleProps extends React.HTMLAttributes<HTMLParagraphElement>, VariantProps<typeof TitleVariants> {
+// const TitleVariants = cva(
+//   'text-xl md:text-3xl lg:text-4xl font-bold',
+//   {
+//     variants: {
+//       variant: {
+//         default: 'text-primary text-start',
+//         doubleBorder: '',
+//       }
+//     },
+//     defaultVariants: {
+//       variant: 'default',
+//     }
+//   }
+// )
+interface TitleProps extends React.HTMLAttributes<HTMLParagraphElement> {
   children: React.ReactNode
+  variant: any
 }
 const Title: FC<TitleProps> = ({ children, variant, className, ...rest }) => {
   return <p
-    className={cn(TitleVariants({ variant, className }))}
+    className={cn(className)}
     {...rest}
   >
     {children}

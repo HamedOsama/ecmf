18c2d1a56b79688cdb6728fa281526f7fc5a702c
Hamed Osama <hamedosama19@gmail.com>
2023-06-26 17:10:09 +0300

    update LocaleSwitcher

--- src/components/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
 import { FC } from 'react'
 
@@ -12,18 +11,18 @@ const LocaleSwitcher: FC<LocaleSwitcherProps> = ({ locale }) => {
   const router = useRouter();
   const pathname = usePathname();
 
-  return <Link
-    // onClick={() => {
-    //   const splited = pathname.split('/')
-    //   const newPathname = splited.filter((item: string) => item !== locale)
-    //   router.replace(`/${locale === 'ar' ? 'en' : 'ar'}${newPathname.join('/')}`)
-    // }}
-    href={`/${locale === 'ar' ? 'en' : 'ar'}${pathname.split('/').filter((item: string) => item !== locale).join('/')}`}
+  return <p
+    onClick={() => {
+      const splited = pathname.split('/')
+      const newPathname = splited.filter((item: string) => item !== locale)
+      router.replace(`/${locale === 'ar' ? 'en' : 'ar'}${newPathname.join('/')}`)
+      router.refresh()
+    }}
   >
     <span className='text-white text-sm font-medium hover:text-gray-400 duration-200 cursor-pointer'>
       {locale === 'ar' ? 'English' : 'العربية'}
     </span>
-  </Link>
+  </p>
 }
 
 export default LocaleSwitcher

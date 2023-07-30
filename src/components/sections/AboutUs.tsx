459e7fe255d14fc5431839228bd5e183bc7d0ec0
Hamed Osama <hamedosama19@gmail.com>
2023-07-30 03:05:58 +0300

    update about us images

--- src/components/sections/AboutUs.tsx
+++ src/components/sections/AboutUs.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image'
 import { FC } from 'react'
 
-import aboutUsImg from '@/images/about.png'
+import aboutUsImg from '@/images/about-us.png'
+import visionImg from '@/images/vision.png'
 import Title from '../UI/typography/Title'
 import About from './About'
 import { useTranslations } from 'next-intl'
@@ -24,7 +25,7 @@ const AboutUs: FC<AboutUsProps> = ({ }) => {
       title={t('ourVision')}
       titleVariant='doubleBorder'
       body={t('ourVisionDescription').replaceAll('\n', '<br>')}
-      img={aboutUsImg}
+      img={visionImg}
       direction='right'
     />
   </section>

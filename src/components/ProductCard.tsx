a78fe453d155f133796c9a46d40275dcc8cd9a05
Hamed Osama <hamedosama19@gmail.com>
2023-07-30 03:16:13 +0300

    update image style

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -12,7 +12,7 @@ interface ProductCardProps extends React.HTMLAttributes<HTMLDivElement> {
 
 const ProductCard: FC<ProductCardProps> = ({ title, description, image, price, currency, className }) => {
   return <div className={cn(
-    'pb-4 sm:pb-8 rounded-2xl bg-[#DDDDDD] hover:bg-[#1F96D32E] duration-300 cursor-pointer',
+    'pb-4 sm:pb-8 rounded-3xl bg-[#DDDDDD] hover:bg-[#1F96D32E] duration-300 cursor-pointer',
     className
   )}
   >
@@ -21,7 +21,7 @@ const ProductCard: FC<ProductCardProps> = ({ title, description, image, price, c
       alt={title}
       width={image.width}
       height={image.height}
-      className='w-full h-auto rounded-2xl'
+      className='w-full h-auto rounded-3xl'
     />
     <div className="flex flex-col gap-1 mt-1 px-4 sm:px-8">
       <p className='text-black text-base sm:text-2xl font-bold'>{title}</p>
